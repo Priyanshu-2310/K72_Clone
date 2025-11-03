bf6f2d04eea765383ac48b060017bae16ab991ec
Priyanshu Verma <priyanshuverma7245@gmail.com>
2025-11-04 00:00:54 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <div className='h-screen w-screen bg-red-900'>
+
+        </div>
+    </div>
+  )
+}
+
+export default Home
