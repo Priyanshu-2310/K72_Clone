06c635c1250124f9734023bcd92041813b3c319c
Priyanshu Verma <priyanshuverma7245@gmail.com>
2025-11-08 22:01:31 +0530

    hello

---
+++ src/Componets/Navigations/FullScreenNav.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const FullScreenNav = () => {
+  return (
+    <div>
+        <div>
+            <div>
+                <h1>Projects</h1>
+                <div>
+                    <h2>Pour Tout voir</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default FullScreenNav
