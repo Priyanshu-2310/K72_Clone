f2c382470f0adc1fe61774e72abf45eae467ebd8
Priyanshu Verma <priyanshuverma7245@gmail.com>
2025-11-15 22:17:39 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,20 @@ import Home from "./pages/Home";
 import Agence from "./pages/Agence";
 import Projects from "./pages/Projects";
 import Navbar from "./Componets/Navigations/Navbar";
+import FullScreenNav from "./Componets/Navigations/FullScreenNav";
 
 
 const App = () => {
   
   return (
     <div className="text-white">
-      <Navbar />
-      <Routes>
+      {/* <Navbar /> */}
+      <FullScreenNav />
+      {/* <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/agence" element={<Agence />} />
         <Route path="/projects" element={<Projects />} />
-      </Routes>
+      </Routes> */}
     </div>
   );
 };
--- src/Componets/Navigations/FullScreenNav.jsx
+++ src/Componets/Navigations/FullScreenNav.jsx
@@ -1,18 +1,58 @@
-import React from 'react'
+import React from "react";
 
 const FullScreenNav = () => {
   return (
-    <div>
-        <div>
-            <div>
-                <h1>Projects</h1>
-                <div>
-                    <h2>Pour Tout voir</h2>
-                </div>
+    <div
+      id="fullscreennav"
+      className="h-screen py-16 w-full absolute bg-amber-900"
+    >
+      <div id="all-links" className="">
+        <div className="relative  link  border-t-[0.5px]">
+          <h1 className="font-[font2] text-[7vw] leading-[1] pt-3 text-center  uppercase">
+            Projects
+          </h1>
+          <div className="absolute overflow-x-auto  top-0 text-black bg-[#D3FD50] flex gap-10">
+            <div className="flex w-max flex-nowrap  items-center gap-5">
+              <h2 className="font-[font2] whitespace-nowrap text-[7vw] leading-[1] pt-3 uppercase">
+                Pour Tout voir
+              </h2>
+              <img
+                className="h-24 w-60 rounded-full"
+                src="/Images/nav1.jpg"
+                alt=""
+              />
+              <h2 className="font-[font2] whitespace-nowrap text-[7vw] leading-[1] pt-3 uppercase">
+                Pour Tout voir
+              </h2>
+              <img
+                className="h-24 w-60 rounded-full"
+                src="/Images/nav2.jpg"
+                alt=""
+              />
             </div>
+            <div className="flex w-max flex-nowrap  items-center gap-5">
+              <h2 className="font-[font2] whitespace-nowrap text-[7vw] leading-[1] pt-3 uppercase">
+                Pour Tout voir
+              </h2>
+              <img
+                className="h-24 w-60 rounded-full"
+                src="/Images/nav1.jpg"
+                alt=""
+              />
+              <h2 className="font-[font2] whitespace-nowrap text-[7vw] leading-[1] pt-3 uppercase">
+                Pour Tout voir
+              </h2>
+              <img
+                className="h-24 w-60 rounded-full"
+                src="/Images/nav2.jpg"
+                alt=""
+              />
+            </div>
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FullScreenNav
+export default FullScreenNav;
